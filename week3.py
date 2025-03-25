d53f5c25ee458273d4701e3750a9112206acce3f
Benard-Okoth <abwaobenard1@gmail.com>
2025-03-25 15:56:26 +0300

    Displayed final result

--- week3.py
+++ week3.py
@@ -14,4 +14,13 @@ def calculate_discount(price, discount_percent):
 original_price = float(input("Enter the original price of the item: "))
 discount_percent = float(input("Enter the discount percentage (20% or higher): "))
 
+# calculate the final price
 final_price = calculate_discount(original_price, discount_percent)
+
+# print the final price after applying the discount or the original price if no discount was applied
+if final_price == original_price:
+    print(f"No discount was applied. The original price is: {original_price}")
+else:    
+    print(f"The final price after applying the discount is: {final_price}") 
+          
+
